c2a094966355dce320b462bb12c79d885d2a3a6c
Alibabbba <44174956+Alibabbba@users.noreply.github.com>
2021-05-23 16:06:00 +0200

    Add files via upload

--- licencjat_ML.py
+++ licencjat_ML.py
@@ -17,36 +17,41 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.cluster import DBSCAN
 from sklearn.model_selection import cross_val_score
 
-
 from sklearn.neighbors import NearestNeighbors
 from sklearn.decomposition import PCA
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, RandomOverSampler
+from sklearn.model_selection import GridSearchCV
+
 
 def ready_data(df):
     df = df.drop(["nazwa"], axis=1)
     X = df.drop(["klasa"], axis=1)
-
-    scaler = preprocessing.StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-    X_normalized = preprocessing.normalize(X_scaled)
-    X_normalized = pd.DataFrame(X_normalized)
-    return X_normalized
-
-def ready_classification(df):
-    X_normalized = ready_data(df)
     df_class = df["klasa"].astype("str")
     label_encoder = preprocessing.LabelEncoder()
     Y = label_encoder.fit_transform(df_class)
+
+    ros = RandomOverSampler()
+    X, Y = ros.fit_resample(X, Y)
+
+    scaler = preprocessing.StandardScaler()
+    X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=X.columns)
+    X_normalized = pd.DataFrame(preprocessing.normalize(X_scaled), columns=X.columns)
+
+    return X_normalized, Y
+
+
+def ready_classification(df):
+    X_normalized, Y = ready_data(df)
     train_X, test_X, train_Y, test_Y = model_selection.train_test_split(X_normalized, Y, random_state=0)
     return train_X, test_X, train_Y, test_Y
 
 
 def ready_clustering(df):
-    X_normalized = ready_data(df)
-    pca = PCA(n_components=3)
+    X_normalized, _ = ready_data(df)
+    pca = PCA(n_components=2)
     X_reduced = pca.fit_transform(X_normalized)
     X_reduced = pd.DataFrame(X_reduced)
-    X_reduced.columns = ['P1', 'P2', "P3"]
+    X_reduced.columns = ['P1', 'P2']
     print(abs(pca.components_))
     print("Ile wyjasnia", pca.explained_variance_ratio_, sum(pca.explained_variance_ratio_))
 
@@ -55,23 +60,34 @@ def ready_clustering(df):
 
 def random_forest_class(X_train, X_test, Y_train, Y_test):
     randomforest = RandomForestClassifier()
-    rfc = pipeline.Pipeline(steps=[('classifier', randomforest)])
-    rfc.fit(X_train, Y_train)
-    Y_pred_rfc = rfc.predict(X_test)
-
-    disp_confusion_matrix(Y_test, Y_pred_rfc, labels=labels)
+    randomforest.fit(X_train, Y_train)
+    param_grid_forest = {'bootstrap': [False, True], 'n_estimators': [2, 4, 6],
+                  'max_features': [2, 3, 4], 'max_leaf_nodes': [5, 50, 500, 5000]}
+    # Y_pred_rfc = rfc.predict(X_test)
+    
+    grid = GridSearchCV(randomforest, param_grid_forest, cv=10, scoring='neg_mean_squared_error')
+    grid.fit(X_train, Y_train)
+    print(f"Best parametrs: {grid.best_estimator_}")
+    grid_predictions = grid.predict(X_test) 
+    
+    disp_confusion_matrix(Y_test, grid_predictions, labels=labels)
     plt.title("Confusion Matrix - RandomForest")
     print(f"Training: imput features = {X_train.shape}, output class = {Y_train.shape}")
     print(f"Testing: imput features = {X_test.shape}, output class = {Y_test.shape}")
     score = cross_val(randomforest, df)
     print(f"Cross Validation(RMSE) scores: {score}, Avreges: {score.mean()}")
     plt.show()
-
     randomforest.fit(X_train, Y_train)
+    param_grid_forest = {'bootstrap': [False, True], 'n_estimators': [2, 4, 6],
+                  'max_features': [2, 3, 4], 'max_leaf_nodes': [5, 50, 500, 5000]}
+
+
+
+        
     plt.barh(columns_names, randomforest.feature_importances_)
     plt.title("Feature importance - RandomForest")
     plt.show()
-    return Y_pred_rfc, Y_test
+    return grid_predictions, Y_test
 
 
 def kneighborsClass(X_train, X_test, Y_train, Y_test):
@@ -105,13 +121,27 @@ def cross_val(classifier, df):
     Y = label_encoder.fit_transform(df_class)
 
     classifier_score = np.sqrt(-1 * cross_val_score(
-        classifier, X, Y, scoring="neg_mean_squared_error", cv=3))
+        classifier, X, Y, scoring="neg_mean_squared_error"))
     return classifier_score
+
+
+def plot_2d_space(X, y, label='Classes'):
+    colors = ["r", 'g']
+    markers = ['o', 's']
+    for l, c, m in zip(np.unique(y), colors, markers):
+        plt.scatter(
+            X[y == l, 0],
+            X[y == l, 1],
+            c=c, label=l, marker=m
+        )
+    plt.title(label)
+    plt.legend(loc='upper right')
+    plt.show()
 
 
 if __name__ == "__main__":
     path = "C:/Users/Mikołaj/Desktop/Licencjat/"
-    df = pd.read_csv(path + "out_df")
+    df = pd.read_csv(path + "out_df")    
     brain_db = pd.read_excel(path + "BRAIN_database_UG_26_02_2021.xlsx")
     brain_picked = brain_db.loc[brain_db["Participant's code"].isin(df["nazwa"])]
     brain_rest = df.loc[~(df['nazwa'].isin(brain_picked["Participant's code"]))]
@@ -119,6 +149,13 @@ if __name__ == "__main__":
     brain_rest = brain_db.loc[brain_db["Participant's code"].isin(brain_rest)]
     brain_picked = brain_picked.append(brain_rest).reset_index(drop=True)
     brain_picked["SEX"] = brain_picked['SEX'].fillna(0)
+
+    # df.klasa.value_counts().plot(kind='bar', title='Count classes')
+    # plt.show()
+    # X_normalized, Y = ready_data(df)
+    # X_normalized["klasa"] = Y
+    # X_normalized.klasa.value_counts().plot(kind='bar', title='Count classes')
+    # plt.show()
 
 # %%
     X_clust = ready_clustering(df)
@@ -151,18 +188,16 @@ if __name__ == "__main__":
     g = plt.scatter(X_clust['P1'], X_clust['P2'], color='g')
     plt.close()
 
-
     markers = ["x", 'o']
-    for i, marker in enumerate(markers):  
+    for i, marker in enumerate(markers):
         plt.scatter(X_clust['P1'].loc[df['klasa'].str.startswith(str(i+1), na=False)],
                     X_clust['P2'].loc[df['klasa'].str.startswith(str(i+1), na=False)],
-                    marker = marker)
+                    marker=marker)
     plt.legend((r, g), ('Label 0', 'Label 1'))
     plt.show()
-    
+
     print(len(X_clust['P1'].loc[df['klasa'].str.startswith('1', na=False)]))
 # %%
-    df_test = df.iloc[0:2]
     labels = ["1A", "1B", "2A", "2B", "2C"]
     columns_names = ['frequency', 'oddech_na_min', 'R_do_2sec', 'R_na_min', 'entropia_oddech', 'entropia_onsety']
     X_train, X_test, Y_train, Y_test = ready_classification(df)
@@ -170,12 +205,15 @@ if __name__ == "__main__":
     print("Random Forest")
     random_forest_class(X_train, X_test, Y_train, Y_test)
     print("=============================")
-    print("KNeighborsClass")
-    kneighborsClass(X_train, X_test, Y_train, Y_test)
-# %%
+    # print("KNeighborsClass")
+    # kneighborsClass(X_train, X_test, Y_train, Y_test)
+    # print("=============================")
 
-    X_normalized = ready_data(df)
+
+# %%
+    X_normalized, Y = ready_data(df)
+    X_normalized["klasa"] = Y
     print(X_normalized)
-    
-    sns.scatter()
+
+    sns.scatterplot(x="R_na_min", y="oddech_na_min", data=X_normalized, hue="klasa")
 
