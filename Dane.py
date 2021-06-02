2fd073d3a00187cd1692c60e8664ee5d2d8a7a79
Alibabbba <44174956+Alibabbba@users.noreply.github.com>
2021-06-02 15:34:18 +0200

    Add files via upload

--- Dane.py
+++ Dane.py
@@ -9,8 +9,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import math
 
 from scipy.signal import periodogram, find_peaks
+
 
 # =======================================
 #   Różnica min/max oddechu
@@ -18,61 +20,63 @@ from scipy.signal import periodogram, find_peaks
 #   !Done! Periodogram usrednic
 #   !Done! Znalezc onset oddechu
 #   Znalezc oddech zasypiajacych osob
-#   Podzielic hbeat na klasy
-#   opisać jak rozóżniałem ręcznie i dlaczego lub ucinać
-#   Synchrogramy i korelogramy te drugie
-#   Zrobić kalsyfikacje
-#   entropia histogramów (równych co 10%)
-#   sprawdzić podział w bezwzględnym czasie (np co 0.5/0.4sec)
+#   !DONE! Podzielic hbeat na klasy
+#   !DEL! opisać jak rozóżniałem ręcznie i dlaczego lub ucinać
+#   !DEL! Synchrogramy i korelogramy te drugie
+#   !Done! Zrobić kalsyfikacje
+#   !Done! entropia histogramów (równych co 10%)
+#   !Done! sprawdzić podział w bezwzględnym czasie (np co 0.5/0.4sec)
+
+#   cechy charakterystyczne: entropia, ilosc oddechow na minutę, frequency, ilosc R (maksymalna frequency)
+#   ilsc RR w pierwszej poltora sekundzie / ilosc oddechow, dodac z tytulu czy zdrowy czy chory (2a - nadcisnienie, 2b - bardzo chory)
+#   ilsc R na minute (suma / 20),
+
+#   powinny wyjsc 2 klastry
+
+#   opisac w preprocesingu ze jeden plik w był opuszczony
 # =======================================
 
-test = True
 
-
-def prepare_data(df, path):
+def prepare_data(df):
     # usuwam 3 sekundy zeby nie bylo nan
     df.drop(df.head(3000).index, inplace=True)
     df.drop(df.tail(3000).index, inplace=True)
     df.reset_index(drop=False, inplace=True)
 
-    df['Hbeat'] = df['Hbeat'].astype(float, errors='raise')
-
-    # df["Pierwsza_pochodna"] = np.gradient(df["Breath"].to_numpy())
-
+    df['Hbeat'] = df['Hbeat'].astype(float)
     df["Pierwsza_pochodna"] = df["Breath"].diff() / 0.001
 
     peaks, _ = find_peaks(-df["Breath"], distance=900, prominence=0.05)
 
-    # print(peaks)
-    lst, poch_more3, poch_less3 = [],[],[]
-    for peak in peaks:
-        df_2 = df[peak: peak+200]
-        lst.append(df_2['Pierwsza_pochodna'].max())
-        if df_2[df_2['Pierwsza_pochodna'] > 0.2].empty:
-            poch_more3.append(1)
-        else:
-            poch_less3.append(1)
-    # print(lst)    
-    print(len(poch_more3), len(poch_less3))
-    print(sum(lst) / len(lst))
-   
+    for index, peak in enumerate(peaks):
+        df_2 = df[peak: peak+400]
+        if not df_2[df_2["Pierwsza_pochodna"] > 0.3].empty:
+            peaks[index] = df_2.loc[
+                df_2["Pierwsza_pochodna"] > 0.3, "index"].iloc[0] - 3000
+        elif not df_2[df_2["Pierwsza_pochodna"] > 0.2].empty:
+            peaks[index] = df_2.loc[
+                df_2["Pierwsza_pochodna"] > 0.2, "index"].iloc[0] - 3000
+        elif not df_2[df_2["Pierwsza_pochodna"] > 0.1].empty:
+            peaks[index] = df_2.loc[
+                df_2["Pierwsza_pochodna"] > 0.1, "index"].iloc[0] - 3000
 
     df["Onset_breath"] = 0
     df["Hbeat_time"] = np.nan
-    time1 = df["Time"].iloc[0]
-    time2 = df["Time"].iloc[peaks[0]]
-
-    # df = rank_hbeat_onset(df, time1, time2)
 
     for index, peak in enumerate(peaks):
-        df.iloc[peak, df.columns.get_loc("Onset_breath")] = df["Breath"].iloc[peak]
+        if index == 0:
+            time1 = df["Time"].iloc[0]
+            time2 = df["Time"].iloc[peaks[0]]
+            df = rank_hbeat_onset(df, time1, time2)
 
+        # nadaje wartosc Breath o indeksie peak, indeksowi peak w kolumnie Onset_breath
+        df.iloc[peak, df.columns.get_loc("Onset_breath")] = df["Breath"].iloc[peak]
         time1 = df["Time"].iloc[peak]
         if index == len(peaks) - 1:
             time2 = df["Time"].iloc[-1]
         else:
             time2 = df["Time"].iloc[peaks[index+1]]
-        # df = rank_hbeat_onset(df,  time1, time2)
+        df = rank_hbeat_onset(df,  time1, time2)
     return df
 
 
@@ -92,8 +96,7 @@ def rank_hbeat_onset(df, begin, end):
     for index, row in hbet.iterrows():
         previous_time = 0
         for time in ranks:
-            if ((row["Time"] - begin) <= time) and ((row["Time"] - begin) >
-                                                    previous_time):
+            if ((row["Time"] - begin) <= time) and ((row["Time"] - begin) > previous_time):
                 df.loc[index, "Hbeat_time"] = time
 
             previous_time = time
@@ -120,7 +123,6 @@ def show_sliced_graph(database, start, how_much):
                     ax=ax, color="r")
     sns.scatterplot(x="Time", y="Breath", data=onset_breath, ax=ax,
                     color="g", s=200, marker="X")
-    
     plt.show()
     return fig
 
@@ -159,6 +161,7 @@ def show_sliced_graph_movable(database):
 def save_sliced_graph(graph, name):
     os.chdir("C:/Users/Mikołaj/Desktop/Licencjat/Pics")
     fig = graph
+    fig.suptitle(name)
     fig.savefig(name + '.png')
     plt.close()
 
@@ -173,64 +176,161 @@ def find_NaN(df, column):
             print(index)
 
 
-def frequency_mean(df_column, limit):
-    asds = df_column.to_numpy()
-    window_size = 10
-    f, widmo = periodogram(asds, fs=1000)
-    f = f[0:-window_size]
-    widmo_mean = []
-    for i in range(len(widmo) - window_size):
-        widmo_mean.append(np.mean(widmo[i:i + window_size]))
+def onset_count(df):
+    hist_size = len(pd.unique(df['Hbeat_time'])) - 1
+    bars_height = df.Hbeat_time.dropna().to_numpy()
 
-    plt.plot(f, widmo_mean)
-    plt.xlim(0, limit)
-    plt.ylabel('Amplituda Widma Oddechu')
-    plt.xlabel('Hz')
-    return None
+    fig, ax = plt.subplots()
+    sns.kdeplot(data=df, x="Hbeat_time", ax=ax, clip=(0.5, hist_size * 0.5))
+    bar_values = ax.hist(bars_height, bins=hist_size,
+                         density=True, histtype='bar', ec='black')[0]
+    ent = -(bar_values*np.log(bar_values))
+    ent = sum([0 if math.isnan(i) else i for i in ent])
+    ax.set_title(f'Onset count: {np.count_nonzero(df["Onset_breath"]) - 1}, Entropia: {round(ent, 4)}')
+    return fig
 
 
-if __name__ == "__main__":
-    path = "C:/Users/Mikołaj/Desktop/Licencjat/Dane/"
-    txt_file_list = os.listdir(path)
-    txt_file_single = txt_file_list[1]
+def onset_count_entropy(df_Hbeat_time):
+    hist_size = len(pd.unique(df_Hbeat_time)) - 1
+    bars_height = df_Hbeat_time.dropna().to_numpy()
+    hist = np.histogram(bars_height, bins=hist_size, density=True)[0]
+    with np.errstate(divide='raise'):
+        try:
+            ent = sum(-(hist*np.log(hist)))
+        except FloatingPointError:
+            hist = [1 if i == 0 else i for i in hist]
+            ent = sum(-(hist*np.log(hist)))
+    return ent
 
-    df = pd.read_csv(path + txt_file_single, delimiter="\t", header=None,
-                      names=["Time", "Spasm", "Breath", 'Hbeat'])
 
-    # df = df.iloc[:16000 + 16000]
-    df = prepare_data(df, path)
-#%%
-    # for txt in txt_file_list:
-    #     df = pd.read_csv(path + txt, delimiter="\t", header=None,
-    #                       names=["Time", "Spasm", "Breath", 'Hbeat'])
-    #     df = prepare_data(df, path)
-    
-    # fig, ax = plt.subplots()
-    # sns.histplot(df, x="Hbeat_time",kde=True, ax=ax, binwidth = 0.5)
-    # ax.set_title(f'Onset count: {np.count_nonzero(df["Onset_breath"]) - 1}')
-    # fig.suptitle(txt_file_single)
-    #     save_sliced_graph(fig, txt)
+def plot_deriv_breath_onset(df, szukana_granica, min_sec, max_sec):
+    df2 = df.iloc[(min_sec-3) * 1000:(max_sec-3) * 1000]
 
-    # name = txt_file[1] + ".xlsx"
-    # with pd.ExcelWriter("C:/Users/Mikołaj/Desktop/Licencjat/Dane_obrobione/" + name) as writer:
-    #     df[:500000].to_excel(writer, sheet_name='first 500k')
-    #     df[500000:1000000].to_excel(writer, sheet_nadddddddddddddddddddddd
-    
-    # frequency_mean(df['Breath'],0.5)
-    # show_sliced_graph(df, 5, 12)
-    # show_sliced_graph_movable(df)
-    # save_sliced_graph(df, 3, 80, txt_file[-1])
-    
-    df2 = df.iloc[5000:16000]
-    prog = 0.1
-
+    prog = szukana_granica
     fig, axs = plt.subplots(2)
     sns.lineplot(x="Time", y="Breath", data=df2, ax=axs[0])
     sns.lineplot(x="Time", y="Pierwsza_pochodna", data=df2, ax=axs[1])
     a = df2.loc[df["Onset_breath"] != 0, "Time"].to_numpy()
-    granica = axs[1].axhline(y=prog, color = "g")
+    granica = axs[1].axhline(y=prog, color="g")
     for i in axs:
         for j in a:
             onset = i.axvline(j, color="red")
             onset_plus = i.axvline(j+0.4, color="black")
-    fig.legend([granica,onset,onset_plus],[str(prog)+" granica","Onset","Onset + 400"])
+    fig.legend([granica, onset, onset_plus], [str(prog)+" granica", "Onset", "Onset + 400"])
+    return fig
+
+
+def frequency_mean(df_column, limit, plot=True):
+    asds = df_column.to_numpy()
+    window_size = 50
+    f, widmo = periodogram(asds, fs=1000)
+    f = f[0:1000-window_size]
+    widmo = widmo[0:1000]
+    widmo_mean = []
+    for i in range(len(widmo) - window_size):
+        widmo_mean.append(np.mean(widmo[i:i + window_size]))
+    if plot is True:
+        fig = plt.figure()
+        plt.plot(f, widmo_mean)
+        plt.xlim(0, limit)
+        plt.ylabel('Amplituda Widma Oddechu')
+        plt.xlabel('Hz')
+        plt.title(f"Max = {f[widmo_mean.index(max(widmo_mean))]}")
+        return widmo_mean, f, fig
+    else:
+        return widmo_mean, f
+
+
+def prepare_outcome_df(path):
+    outcome_df = pd.DataFrame(
+        columns=["nazwa", "entropia_onsety", "entropia_oddech",
+                 "frequency", "oddech_na_min", "R_do_2sec", "R_na_min", "klasa"])
+    txt_file_list = os.listdir(path)
+    for index, file_name in enumerate(txt_file_list):
+        in_df = pd.read_csv(path + file_name, delimiter="\t", header=None,
+                            names=["Time", "Spasm", "Breath", 'Hbeat'])
+        in_df = prepare_data(in_df)
+        widmo, f = frequency_mean(in_df['Breath'], 0.5, plot=False)
+        entropia_oddech_list = []
+        window_size = int(len(widmo)/11)
+        for i in range(0, len(widmo)-window_size, window_size):
+            entropia_oddech_list.append(np.mean(widmo[i:i+window_size]))
+
+            entropia_onsety = onset_count_entropy(in_df["Hbeat_time"])
+        entropia_oddech = sum(-(entropia_oddech_list*np.log(entropia_oddech_list)))
+        frequency = f[widmo.index(max(widmo))]
+        oddechy_na_min = np.count_nonzero(in_df["Onset_breath"])
+        R_do_2sec = in_df.loc[(in_df["Hbeat"] != 0) & (in_df["Hbeat_time"] < 2), "Hbeat_time"].count()
+        R_do_2sec = R_do_2sec / oddechy_na_min
+        R_na_min = in_df.loc[in_df["Hbeat"] != 0, "Hbeat"].size / 20
+        oddechy_na_min = oddechy_na_min / 20
+        klasa = file_name[4:6]
+
+        if int(file_name[4]) == 1:
+            name = "I" + file_name[5] + " " + file_name[7:10]
+            # klasa = '1'
+        else:
+            name = "II" + file_name[5] + " " + file_name[7:10]
+            # klasa = "2"
+
+        outcome_df = outcome_df.append({
+            "nazwa": name,
+            "entropia_onsety": entropia_onsety,
+            "entropia_oddech": entropia_oddech,
+            "frequency": frequency,
+            "oddech_na_min": oddechy_na_min,
+            "R_do_2sec": R_do_2sec,
+            "R_na_min": R_na_min,
+            "klasa": klasa
+              }, ignore_index=True)
+        print(f"Done {index+1} out of {len(txt_file_list)}")
+    return outcome_df
+
+
+if __name__ == "__main__":
+    path = "C:/Users/Mikołaj/Desktop/Licencjat/Dane/"
+    out_path = "C:/Users/Mikołaj/Desktop/Licencjat/"
+    txt_file_list = os.listdir(path)
+    txt_file_single = txt_file_list[3]
+
+    df = pd.read_csv(path + txt_file_single, delimiter="\t", header=None,
+                     names=["Time", "Spasm", "Breath", 'Hbeat'])
+
+    # df = df.iloc[:16000 + 16000]
+    # df = prepare_data(df)
+
+    # widmo, f = frequency_mean(df['Breath'], 0.5, plot=False)
+
+    # lis = []
+    # window_size = int(len(widmo)/11)
+    # for i in range(0,len(widmo)-window_size , window_size):
+    #     lis.append(np.mean(widmo[i:i+window_size]))
+    # print(sum(-(lis*np.log(lis))))
+
+    # print(onset_count_entropy(df["Hbeat_time"]))
+    # onset_count(df.Hbeat_time)
+    # frequency_mean(df['Breath'],0.5)
+    # widmo, f, fig = frequency_mean(df['Breath'], 0.5, plot=True)
+    # frequency = f[widmo.index(max(widmo))]
+    # print(frequency)
+
+    # show_sliced_graph(df, 5, 12)
+    # show_sliced_graph_movable(df)
+    # save_sliced_graph(df, 3, 80, txt_file[-1])
+    # data_info(df)
+    # plot_deriv_breath_onset(df, 0.1, 8, 19)
+    # print(onset_count_entropy(df["Hbeat_time"]))
+
+    # for txt in txt_file_list:
+    #     df = pd.read_csv(path + txt, delimiter="\t", header=None,
+    #                       names=["Time", "Spasm", "Breath", 'Hbeat'])
+    #     df = prepare_data(df)
+    #     widmo, f, fig = frequency_mean(df['Breath'], 0.5, plot=True)
+    #     frequency = f[widmo.index(max(widmo))]
+
+    #     save_sliced_graph(fig, txt)
+
+    test_path = "C:/Users/Mikołaj/Desktop/Licencjat"
+    out_df = prepare_outcome_df(path)
+    out_df.to_csv(path_or_buf=out_path + "out_df", index=False)
+    print(out_df)
--- licencjat_ML.py
+++ licencjat_ML.py
@@ -9,17 +9,18 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
-from sklearn import preprocessing, pipeline, model_selection
+from sklearn import preprocessing, model_selection
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-from sklearn.cluster import DBSCAN
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, matthews_corrcoef
 from sklearn.model_selection import cross_val_score
 
-from sklearn.neighbors import NearestNeighbors
 from sklearn.decomposition import PCA
-from imblearn.over_sampling import SMOTE, RandomOverSampler
+from imblearn.over_sampling import RandomOverSampler
 from sklearn.model_selection import GridSearchCV
 
 
@@ -42,7 +43,7 @@ def ready_data(df):
 
 def ready_classification(df):
     X_normalized, Y = ready_data(df)
-    train_X, test_X, train_Y, test_Y = model_selection.train_test_split(X_normalized, Y, random_state=0)
+    train_X, test_X, train_Y, test_Y = model_selection.train_test_split(X_normalized, Y)
     return train_X, test_X, train_Y, test_Y
 
 
@@ -62,14 +63,15 @@ def random_forest_class(X_train, X_test, Y_train, Y_test):
     randomforest = RandomForestClassifier()
     randomforest.fit(X_train, Y_train)
     param_grid_forest = {'bootstrap': [False, True], 'n_estimators': [2, 4, 6],
-                  'max_features': [2, 3, 4], 'max_leaf_nodes': [5, 50, 500, 5000]}
+                         'max_features': [2, 3, 4], 'max_leaf_nodes': [5, 50, 500, 5000]}
     # Y_pred_rfc = rfc.predict(X_test)
-    
-    grid = GridSearchCV(randomforest, param_grid_forest, cv=10, scoring='neg_mean_squared_error')
+
+    grid = GridSearchCV(randomforest, param_grid_forest,
+                        cv=10, scoring='neg_mean_squared_error')
     grid.fit(X_train, Y_train)
     print(f"Best parametrs: {grid.best_estimator_}")
-    grid_predictions = grid.predict(X_test) 
-    
+    grid_predictions = grid.predict(X_test)
+
     disp_confusion_matrix(Y_test, grid_predictions, labels=labels)
     plt.title("Confusion Matrix - RandomForest")
     print(f"Training: imput features = {X_train.shape}, output class = {Y_train.shape}")
@@ -79,11 +81,7 @@ def random_forest_class(X_train, X_test, Y_train, Y_test):
     plt.show()
     randomforest.fit(X_train, Y_train)
     param_grid_forest = {'bootstrap': [False, True], 'n_estimators': [2, 4, 6],
-                  'max_features': [2, 3, 4], 'max_leaf_nodes': [5, 50, 500, 5000]}
-
-
-
-        
+                         'max_features': [2, 3, 4], 'max_leaf_nodes': [5, 50, 500, 5000]}
     plt.barh(columns_names, randomforest.feature_importances_)
     plt.title("Feature importance - RandomForest")
     plt.show()
@@ -92,19 +90,50 @@ def random_forest_class(X_train, X_test, Y_train, Y_test):
 
 def kneighborsClass(X_train, X_test, Y_train, Y_test):
     knnClas = KNeighborsClassifier()
-    knn_model = pipeline.Pipeline(steps=[('classifier', knnClas)])
-    knn_model.fit(X_train, Y_train)
-    Y_pred_knn = knn_model.predict(X_test)
-    
-    print("KNeighborsClass")
-    disp_confusion_matrix(Y_test, Y_pred_knn, labels=labels)
+    knnClas.fit(X_train, Y_train)
+    y_pred = knnClas.predict(X_test)
+
+    disp_confusion_matrix(Y_test, y_pred, labels=labels)
     print(f"Training: imput features = {X_train.shape}, output class = {Y_train.shape}")
     print(f"Testing: imput features = {X_test.shape}, output class = {Y_test.shape}")
     score = cross_val(knnClas, df)
     print(f"Cross Validation(RMSE) scores: {score}, Avreges: {score.mean()}")
     plt.title("Confusion Matrix - KNeighbors")
     plt.show()
-    return Y_pred_knn, Y_test
+    return y_pred, Y_test
+
+
+def decisionTreeClass(X_train, X_test, Y_train, Y_test, tree_plot=False):
+    decision_tree = DecisionTreeClassifier(random_state=0)
+    decision_tree = decision_tree.fit(X_train, Y_train)
+    Y_pred = decision_tree.predict(X_test)
+
+    disp_confusion_matrix(Y_test, Y_pred, labels=labels)
+    print(f"Training: imput features = {X_train.shape}, output class = {Y_train.shape}")
+    print(f"Testing: imput features = {X_test.shape}, output class = {Y_test.shape}")
+    score = cross_val(decision_tree, df)
+    print(f"Cross Validation(RMSE) scores: {score}, Avreges: {score.mean()}")
+    plt.title("Confusion Matrix - DecisionTree")
+    plt.show()
+    if tree_plot is True:
+        plt.figure(figsize=(20, 15))
+        plot_tree(decision_tree, feature_names=X_train.columns, fontsize=8)
+        os.chdir("C:/Users/Mikołaj/Desktop/Licencjat/Pics")
+        plt.savefig('decision_tree.pdf')
+    return Y_pred, Y_test
+
+
+def gaussian_naive(X_train, X_test, Y_train, Y_test):
+    gnb = GaussianNB()
+    Y_pred = gnb.fit(X_train, Y_train).predict(X_test)
+    disp_confusion_matrix(Y_test, Y_pred, labels=labels)
+    print(f"Training: imput features = {X_train.shape}, output class = {Y_train.shape}")
+    print(f"Testing: imput features = {X_test.shape}, output class = {Y_test.shape}")
+    score = cross_val(gnb, df)
+    print(f"Cross Validation(RMSE) scores: {score}, Avreges: {score.mean()}")
+    plt.title("Confusion Matrix - Gaussian Naive")
+    plt.show()
+    return Y_pred, Y_test
 
 
 def disp_confusion_matrix(Y_test, Y_pred, labels=None):
@@ -125,95 +154,39 @@ def cross_val(classifier, df):
     return classifier_score
 
 
-def plot_2d_space(X, y, label='Classes'):
-    colors = ["r", 'g']
-    markers = ['o', 's']
-    for l, c, m in zip(np.unique(y), colors, markers):
-        plt.scatter(
-            X[y == l, 0],
-            X[y == l, 1],
-            c=c, label=l, marker=m
-        )
-    plt.title(label)
-    plt.legend(loc='upper right')
-    plt.show()
-
-
 if __name__ == "__main__":
     path = "C:/Users/Mikołaj/Desktop/Licencjat/"
-    df = pd.read_csv(path + "out_df")    
-    brain_db = pd.read_excel(path + "BRAIN_database_UG_26_02_2021.xlsx")
-    brain_picked = brain_db.loc[brain_db["Participant's code"].isin(df["nazwa"])]
-    brain_rest = df.loc[~(df['nazwa'].isin(brain_picked["Participant's code"]))]
-    brain_rest = [x[0:3] + " op " + x[4:7] for x in brain_rest['nazwa']]
-    brain_rest = brain_db.loc[brain_db["Participant's code"].isin(brain_rest)]
-    brain_picked = brain_picked.append(brain_rest).reset_index(drop=True)
-    brain_picked["SEX"] = brain_picked['SEX'].fillna(0)
+    df = pd.read_csv(path + "out_df")
 
-    # df.klasa.value_counts().plot(kind='bar', title='Count classes')
-    # plt.show()
-    # X_normalized, Y = ready_data(df)
-    # X_normalized["klasa"] = Y
-    # X_normalized.klasa.value_counts().plot(kind='bar', title='Count classes')
-    # plt.show()
-
-# %%
-    X_clust = ready_clustering(df)
-    db = DBSCAN(eps=1.5, min_samples=4)
-    db.fit(X_clust)
-    # print(db.labels_)
-    labels = db.labels_
-
-    neighbors = NearestNeighbors(n_neighbors=4)
-    neighbors_fit = neighbors.fit(X_clust)
-    distances, indices = neighbors_fit.kneighbors(X_clust)
-
-    distances = np.sort(distances, axis=0)
-    distances = distances[:, 1]
-    plt.plot(distances)
-    plt.show()
-
-    colours = {}
-    colours[0] = 'r'
-    colours[1] = 'g'
-    colours[2] = 'b'
-    colours[3] = 'c'
-    colours[4] = 'y'
-    colours[5] = 'm'
-    colours[-1] = 'k'
-
-    cvec = [colours[label] for label in labels]
-
-    r = plt.scatter(X_clust['P1'], X_clust['P2'], color='r')
-    g = plt.scatter(X_clust['P1'], X_clust['P2'], color='g')
-    plt.close()
-
-    markers = ["x", 'o']
-    for i, marker in enumerate(markers):
-        plt.scatter(X_clust['P1'].loc[df['klasa'].str.startswith(str(i+1), na=False)],
-                    X_clust['P2'].loc[df['klasa'].str.startswith(str(i+1), na=False)],
-                    marker=marker)
-    plt.legend((r, g), ('Label 0', 'Label 1'))
-    plt.show()
-
-    print(len(X_clust['P1'].loc[df['klasa'].str.startswith('1', na=False)]))
-# %%
     labels = ["1A", "1B", "2A", "2B", "2C"]
-    columns_names = ['frequency', 'oddech_na_min', 'R_do_2sec', 'R_na_min', 'entropia_oddech', 'entropia_onsety']
+    columns_names = ['frequency', 'oddech_na_min', 'R_do_2sec',
+                     'R_na_min', 'entropia_oddech', 'entropia_onsety']
     X_train, X_test, Y_train, Y_test = ready_classification(df)
 
-    print("Random Forest")
-    random_forest_class(X_train, X_test, Y_train, Y_test)
+    ready_clustering(df)
+    print("Random Forest Classifier")
+    rand_pred, rand_true = random_forest_class(X_train, X_test, Y_train, Y_test)
     print("=============================")
-    # print("KNeighborsClass")
-    # kneighborsClass(X_train, X_test, Y_train, Y_test)
-    # print("=============================")
+    print("KNeighbors Classifier")
+    kn_pred, kn_true = kneighborsClass(X_train, X_test, Y_train, Y_test)
+    print("=============================")
+    print("Decision Tree Classifier")
+    tree_pred, tree_true = decisionTreeClass(X_train, X_test, Y_train, Y_test)
+    print("=============================")
+    print("Naive Bayes Classifier")
+    gaus_pred, gaus_true = gaussian_naive(X_train, X_test, Y_train, Y_test)
 
+    mcc_dic = {
+        "Random Forest": matthews_corrcoef(rand_true, rand_pred),
+        "KNeighbors": matthews_corrcoef(kn_true, kn_pred),
+        "Decision Tree": matthews_corrcoef(tree_true, tree_pred),
+        "Naive Bayes": matthews_corrcoef(gaus_true, gaus_pred)
+        }
 
+    print(mcc_dic)
 # %%
     X_normalized, Y = ready_data(df)
     X_normalized["klasa"] = Y
     print(X_normalized)
 
     sns.scatterplot(x="R_na_min", y="oddech_na_min", data=X_normalized, hue="klasa")
-
